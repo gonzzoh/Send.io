b8d81b0708024a3cea5a3d08ad3c240ad9c2733c
gonzzoh <gmeza0118@gmail.com>
2023-12-29 15:06:38 -0500

    Load Form Complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App.tsx'
+import LoadForm from './components/LoadForm.tsx'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <LoadForm />
   </React.StrictMode>,
 )
 
