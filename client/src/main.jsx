6be1cc57369278fcba344b590f33fcf1d09be865
gonzzoh <gmeza0118@gmail.com>
2024-01-02 17:12:42 -0500

    Post Method Functional, Not Updating, Destination Bug

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LoadForm from './components/LoadForm.tsx'
-import App from './components/ShipmentList.jsx'
+import ShipmentForm from './components/ShipmentForm.tsx'
+import ShipmentList from './components/ShipmentList.jsx'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LoadForm />
-    <App />
+    <ShipmentForm />
+    <ShipmentList />
   </React.StrictMode>,
 )
