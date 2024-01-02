8a79ed523291595874c6ba8527279f7e5f02c012
gonzzoh <gmeza0118@gmail.com>
2024-01-01 23:13:22 -0500

    Minor Bug Fix

--- server/src/controllers/destroy.js
+++ server/src/controllers/destroy.js
@@ -1,7 +1,7 @@
 const destroy = async (req, res) => {
     const { id } = req.params;
     console.log(id)
-    const deletedShipment = await req.Shipment.delete(Number(id));
+    const deletedShipment = await req.Shipments.delete(Number(id));
     deletedShipment
         ? res.status(200).send(deletedShipment)
         : res.sendStatus(500).send({err: 'could not delete shipment'});
