abc50a26566d7a0079ca21da9e37ff82fd1be1ac
gonzzoh <gmeza0118@gmail.com>
2024-01-09 15:28:17 -0500

    Marker Bug Fixed

--- client/src/components/TruckRoutes.jsx
+++ client/src/components/TruckRoutes.jsx
@@ -4,49 +4,13 @@ import "../styles/TruckRoutes.css";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
-/* --------------------------- Display Route on Map -------------------------- */
-const DisplayRoute = ({ routeData, destinations }) => {
-    const map = useMap();
-
-    useEffect(() => {
-        if (routeData.features && routeData.features.length > 0) {
-            try {
-                L.geoJSON(routeData, {
-                    style: (feature) => {
-                        return {
-                            color: "rgba(20, 137, 255, 0.7)",
-                            weight: 5,
-                        };
-                    },
-                    onEachFeature: (feature, layer) => {
-                        if (feature.properties && feature.properties.instruction) {
-                            layer.bindPopup(feature.properties.instruction);
-                        }
-                    },
-                }).addTo(map);
-
-                // Add markers with popups for each destination
-                destinations.forEach((destination, index) => {
-                    const destinationLatLng = L.latLng(destination[1], destination[0]);
-                    const popupContent = `Destination ${index + 1}: ${destination}`;
-
-                    L.marker(destinationLatLng).addTo(map).bindPopup(popupContent);
-                });
-            } catch (error) {
-                console.error("Error displaying route:", error);
-            }
-        } else {
-            console.error("No route data available");
-        }
-    }, [map, routeData, destinations]);
-
-    return null;
-};
 
 export default function TruckRoutes() {
     let [routeData, setRouteData] = useState({});
     let [origin, setOrigin] = useState(["New York, NY"]);
+    let [originCoords, setOriginCoords] = useState([]);
     let [destinations, setDestinations] = useState(["Los Angeles, CA", "Denver, CO"]);
+    let [destinationCoords, setDestinationCoords] = useState([])
     const myAPIKey = "282e5784a0a84ae096ecc0252edc2c4a";
 
     /* ---------------------- Convert Names to Coordinates ---------------------- */
@@ -75,7 +39,7 @@ export default function TruckRoutes() {
         const originCoordinates = await geocodeLocation(origin);
 
         if (originCoordinates) {
-            setOrigin(originCoordinates);
+            setOriginCoords(originCoordinates);
         }
 
         const destinationCoordinates = await Promise.all(
@@ -83,7 +47,7 @@ export default function TruckRoutes() {
         );
 
         if (destinationCoordinates.every(coord => coord !== null)) {
-            setDestinations(destinationCoordinates);
+            setDestinationCoords(destinationCoordinates);
 
             const originCoordString = `${originCoordinates[1]},${originCoordinates[0]}`;
             const destinationCoordStrings = destinationCoordinates.map(coord => `${coord[1]},${coord[0]}`).join('|');
@@ -98,6 +62,52 @@ export default function TruckRoutes() {
         }
     }
 
+    /* --------------------------- Display Route on Map -------------------------- */
+    const DisplayRoute = ({ routeData, originCoords, destinationCoords }) => {
+        const map = useMap();
+
+        useEffect(() => {
+            if (routeData.features && routeData.features.length > 0) {
+                try {
+                    L.geoJSON(routeData, {
+                        style: (feature) => {
+                            return {
+                                color: "rgba(20, 137, 255, 0.7)",
+                                weight: 5,
+                            };
+                        },
+                        onEachFeature: (feature, layer) => {
+                            if (feature.properties && feature.properties.instruction) {
+                                layer.bindPopup(feature.properties.instruction);
+                            }
+                        },
+                    }).addTo(map);
+
+                    const originLatLng = L.latLng(originCoords[1], originCoords[0])
+                    const originContent = `Origin: ${origin}`;
+                    L.marker(originLatLng).addTo(map).bindPopup(originContent);
+
+                    destinationCoords.forEach((destination, index) => {
+                        const destinationLatLng = L.latLng(destination[1], destination[0]);
+                        const destinationContent = `${destinations[index]}`;
+
+                        L.marker(destinationLatLng).addTo(map).bindPopup(destinationContent);
+                    });
+                } catch (error) {
+                    console.error("Error displaying route:", error);
+                }
+            } else {
+                console.error("No route data available");
+            }
+        }, [map, routeData, destinationCoords]);
+
+        return null;
+    };
+    useEffect(() => {
+        // console.log(origin, originCoords);
+        console.log(destinationCoords);
+    })
+
     return (
         <>
             <MapContainer center={[37.0902, -95.7129]} zoom={4}>
@@ -105,7 +115,7 @@ export default function TruckRoutes() {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <DisplayRoute routeData={routeData} destinations={destinations} />
+                <DisplayRoute routeData={routeData} originCoords={originCoords} destinationCoords={destinationCoords} />
             </MapContainer>
             <button onClick={getRoute}>Get Directions</button>
         </>
